d8166ebac88ab30a59f06959ab7eb7bd429670be
Jessica Fatta <jessicafatta@gmail.com>
2022-04-06 11:25:11 -0400

    Begin component build with state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,19 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Card from '../Card'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      tricks: []
+    }
+  }
   render() {
     return (
       <div className="App">
         <h1>Sick Trick Wish List</h1>
+        <Card />
       </div>
     );
   }
