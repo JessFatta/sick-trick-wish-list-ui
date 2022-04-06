d1d3d3683ed8b6cef8fa6f18463ec0fe4f70b857
Jessica Fatta <jessicafatta@gmail.com>
2022-04-06 12:10:57 -0400

    Add component did mount method for fetch and update state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import Card from '../Card'
 import Tricks from '../Tricks'
+import getData from '/Users/jessicafatta/turing/3module/midmod/sick-trick-wish-list-ui/src/apiCalls.js'
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      tricks: [
-        {id: 1, name: 'tricksss'}
-      ]
+      tricks: []
     }
+  }
+
+  componentDidMount() {
+    getData()
+    .then(data => this.setState({tricks: data}))
+    
+    //this.setState({tricks: })
+
   }
   render() {
     return (
