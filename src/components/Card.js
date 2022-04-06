c4750bca23bee90a587a605098725db025654ab0
Jessica Fatta <jessicafatta@gmail.com>
2022-04-06 13:38:04 -0400

    Refactor display

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,8 @@ import './Card.css'
 const Card = ({stance, name, obstacle, tutorial, id}) => {
   return (
     <div className='card'>
-      <p>{stance} {name}</p>
+      <p>{stance}</p>
+      <p>{name}</p>
       <p>Obstacle: {obstacle}</p>
       <p>Link to tutorial: </p> 
       <p>{tutorial}</p>
