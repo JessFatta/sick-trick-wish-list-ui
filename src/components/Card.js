accdb932bc742e383e603392b80472e8504a4909
Jessica Fatta <jessicafatta@gmail.com>
2022-04-06 11:24:19 -0400

    Create file for card js

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Card.css'
+
+const Card = () => {
+  return (
+    <div className='card'>
+
+      <h2>this is the card</h2>
+    </div>
+  )
+}
+
+export default Card
